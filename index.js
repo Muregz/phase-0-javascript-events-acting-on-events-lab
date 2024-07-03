46a1ed4b45c3041dc22c9f823594bf1cf886c8d4
Muregz <fabian.murega@student.moringaschool.com>
2024-07-03 13:45:38 +0300

    master

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+
+function moveDodgerLeft() {    
+    const dodger = document.getElementById('dodger');    
+    let leftPosition = parseInt(dodger.style.left) || 0;   
+    leftPosition = Math.max(0, leftPosition - 1);    
+    dodger.style.left = `${leftPosition}px`;}
+function moveDodgerRight() {    
+    const dodger = document.getElementById('dodger');   
+    let leftPosition = parseInt(dodger.style.left) || 0;  
+    leftPosition = Math.min(window.innerWidth - dodger.clientWidth, leftPosition + 1);   
+    dodger.style.left = `${leftPosition}px`;
+}
